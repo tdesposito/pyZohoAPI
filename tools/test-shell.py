100a1db3a39951a909e5f0ca28b26dddb4b013a2
Todd Esposito <todd@toddesposito.com>
2021-02-25 09:39:20 -0600

    move test shell into tools

--- tools/test-shell.py
+++ tools/test-shell.py
@@ -1,18 +1,18 @@
 # This file is part of pyZohoAPI, Copyright (C) Todd D. Esposito 2021.
 # Distributed under the MIT License (see https://opensource.org/licenses/MIT).
 
-from importlib import reload
 import json
 from pprint import pprint
 import sys
 
+sys.path.insert(0, "..")
+
 if sys.flags.interactive:
     import pyzohoapi
     import private
+    td = private.testdata
 
     def r():
-        # reload(pyzohoapi)
-        # reload(private)
         return pyzohoapi.inventory.ZohoInventory(private.t['orgid'], private.t['region'], **private.t['api'])
 
     def dump(o, key=None):
@@ -29,14 +29,15 @@ if sys.flags.interactive:
 
     print("\nTest shell loaded. Here's what you have:")
     print("\nModules: json;")
-    print("\nFunctions:\n\tpprint();")
-    # print("\tr() -> reloads modules, returns new 'z'")
+    print("\nFunctions:")
+    print("\tpprint();")
+    print("\tdump(object, key=None) -> dumps a Zoho Object (or optional attribute);")
     print("\nObjects:")
-    print("\tprivate.testdata -> dict, aliased to private.t;")
+    print("\tprivate.testdata -> dict, aliased to td;")
     print("\tz -> ZohoInventory object : configured via testdata;")
     print("Enjoy your testing!\n")
     z = r()
 else:
     print("\nThis script bootstraps an interactive test enviroment, but only this way:")
-    print("\t> python -i run-me-interactively.py")
+    print(f"\t> python -i {__file__}")
     print("Otherwise, it's all for naught.\n")

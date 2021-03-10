8c0c6e0f68f8aefa6e5c58fdb124a38070886d6c
RapidCompiler <revmatcher@sanjaysuresh.com>
2021-03-10 20:28:44 +0530

    Added subscriptions.py file and updated README.md Also added the Subscriptions file to be included in the package __init__.py file. But I did not edit the objecttypes/__init__.py file to account for the functions in the subscriptions.py file. This is because there are some keyword arguments namely idform, nameform, mixins and raw whose usage is highly ambigious. I do not know which parameter to use for which function. Please change the concerned file(s), after pulling this code, and do explain which fucntions require which parameters.

--- pyzohoapi/__init__.py
+++ pyzohoapi/__init__.py
@@ -5,7 +5,9 @@ __version__ = '0.4.1'
 __all__ = [
     "ZohoBooks",
     "ZohoInventory",
+    "ZohoSubscriptions"
 ]
 
 from .books import ZohoBooks
 from .inventory import ZohoInventory
+from .subscriptions import ZohoSubscriptions

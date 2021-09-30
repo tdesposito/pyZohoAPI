5189792292d57d61ec268b3cbbd5e1546f31e75a
Todd Esposito <todd@toddesposito.com>
2021-09-30 16:09:12 -0500

    Bumping version: 0.9.0 → 0.9.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pyZohoAPI (v0.9.0)
+# pyZohoAPI (v0.9.1)
  **pyZohoAPI** provides Pythonic access to Zoho APIs in the Finance Plus suite:
  * **Books**
  * *Checkout*<sup>*</sup>
--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Todd D. Esposito'
 author = 'Todd D. Esposito'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.9.1'
 
 
 # -- General configuration ---------------------------------------------------
